b70e1d51a4bf8613975816f3c89be9807709662a
Vitor Henrique -- STHE -- <38897030+STHEe@users.noreply.github.com>
2019-10-11 22:44:30 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,45 +6,47 @@ var cols = (w / size);
 var rows = (h / size);
 
 var caminho = [];
+var personagens = [];
+
+var pintura;
+var ladrao;
+
+var cont = 0;
 
 function setup() {
   createCanvas(w, h);
-
-  let ladrao = gerarPos(rows, cols);
-  let pintura = gerarPos(rows, cols);
-
-  if (ladrao == undefined)
-    ladrao = gerarPos(rows, cols);
-  if (pintura == undefined)
-    pintura = gerarPos(rows, cols);
-
 
   for (let x = 0; x <= rows; x++) {
     let aux = [];
     for (let y = 0; y <= cols; y++) {
       let node = new Node(x, y, size);
-      if (ladrao.x == x && ladrao.y == y) {
-        node.visitada = true;
-        node.ladrao = true;
-        aux.push(node);
-      } else if (pintura.x == x && pintura.y == y) {
-        node.visitada = true;
-        node.pintura = true;
-        aux.push(node);
-      } else {
-        aux.push(node);
-      }
+      aux.push(node);
     }
     grid.push(aux);
   }
-
 }
 
 function draw() {
   background(220);
 
-  let ladrao;
-  let pintura;
+  if (personagens[0] != undefined) {
+    let p = personagens[0];
+    ladrao = new Node(p.x, p.y, size);
+
+    ladrao.visitada = true;
+    ladrao.ladrao = true;
+
+    grid[p.x][p.y] = ladrao;
+  }
+  if (personagens[1] != undefined) {
+    let p = personagens[1];
+    pintura = new Node(p.x, p.y, size);
+
+    pintura.visitada = true;
+    pintura.pintura = true;
+
+    grid[p.x][p.y] = pintura;
+  }
 
   for (let row of grid) {
     for (let node of row) {
@@ -56,13 +58,19 @@ function draw() {
     }
   }
 
-  Procurar(ladrao, pintura);
+  if (personagens.length == 2) {
 
-  frameRate(15);
+    Procurar(ladrao, pintura);
+    frameRate(10);
+
+  }
+
+
+
+
 }
 
 function Procurar(ladrao, pintura) {
-  console.log(ladrao, pintura)
 
   let adj = [];
   let e;
@@ -78,38 +86,23 @@ function Procurar(ladrao, pintura) {
     for (let y in grid[x]) {
       let r = grid[x][y];
 
-      if (ladrao.pos.x + 1 == r.pos.x && ladrao.pos.y == r.pos.y) {
-        //DIREITA
+      if (ladrao.pos.x + 1 == r.pos.x && ladrao.pos.y == r.pos.y)
         d = r;
-      }
-      if (ladrao.pos.x - 1 == r.pos.x && ladrao.pos.y == r.pos.y) {
-        //ESQUERDA        
+      if (ladrao.pos.x - 1 == r.pos.x && ladrao.pos.y == r.pos.y)
         e = r;
-      }
-      if (ladrao.pos.x - 1 == r.pos.x && ladrao.pos.y - 1 == r.pos.y) {
-        //ESQUERDA BAIXO
+      if (ladrao.pos.x - 1 == r.pos.x && ladrao.pos.y - 1 == r.pos.y)
         eb = r;
-      }
-      if (ladrao.pos.x - 1 == r.pos.x && ladrao.pos.y + 1 == r.pos.y) {
-        //ESQUERDA CIMA
+      if (ladrao.pos.x - 1 == r.pos.x && ladrao.pos.y + 1 == r.pos.y)
         ec = r;
-      }
-      if (ladrao.pos.x + 1 == r.pos.x && ladrao.pos.y + 1 == r.pos.y) {
-        //DIREITA BAIXO
+      if (ladrao.pos.x + 1 == r.pos.x && ladrao.pos.y + 1 == r.pos.y)
         db = r;
-      }
-      if (ladrao.pos.x + 1 == r.pos.x && ladrao.pos.y - 1 == r.pos.y) {
-        //DIREITA CIMA
+      if (ladrao.pos.x + 1 == r.pos.x && ladrao.pos.y - 1 == r.pos.y)
         dc = r;
-      }
-      if (ladrao.pos.x == r.pos.x && ladrao.pos.y - 1 == r.pos.y) {
-        // CIMA
+      if (ladrao.pos.x == r.pos.x && ladrao.pos.y - 1 == r.pos.y)
         c = r;
-      }
-      if (ladrao.pos.x == r.pos.x && ladrao.pos.y + 1 == r.pos.y) {
-        // BAIXO
+      if (ladrao.pos.x == r.pos.x && ladrao.pos.y + 1 == r.pos.y)
         b = r;
-      }
+
     }
   }
 
@@ -126,43 +119,43 @@ function Procurar(ladrao, pintura) {
 
   let ordenada = adj.sort((a, b) => (a.d > b.d) ? 1 : -1);
   let melhor = ordenada[0];
-  for (let y = 1; y < ordenada.length; y ++){
+  for (let y = 1; y < ordenada.length; y++) {
     if (ordenada[y] != undefined)
       ordenada[y].sendoAnalisada = false;
   }
 
   if (melhor.sendoAnalisada && melhor.pintura) {
-//     background(125);
+    // background(125);
 
-//     for (let x = 0; x <= rows; x++) {
-//       for (let y = 0; y <= cols; y++) {
-//         rect(x * size, y * size, size, size)
-//         fill(150)
-//       }
-//     }
+    //     for (let x = 0; x <= rows; x++) {
+    //       for (let y = 0; y <= cols; y++) {
+    //         rect(x * size, y * size, size, size)
+    //         fill(150)
+    //       }
+    //     }
 
-//     //desenhar o inicial da pintura
-//     rect(pintura.x, pintura.y, pintura.t, pintura.t);
-//     fill(0, 0, 255);
-//     //desenhar o inicial do ladrao
-//     rect(ladrao.x, ladrao.y, ladrao.t, ladrao.t);
-//     fill(255, 0, 0);
-//     //desenha o caminho melhor
-//     caminho.map(way => {
-//       let node = grid[way.x][way.y];
-//       rect(node.x, node.y, node.t, node.t);
-//       fill(0, 0, 255, 150);
-//     });
+    //     //desenhar o inicial da pintura
+    //     rect(pintura.x, pintura.y, pintura.t, pintura.t);
+    //     fill(0, 0, 255);
+    //     //desenhar o inicial do ladrao
+    //     rect(ladrao.x, ladrao.y, ladrao.t, ladrao.t);
+    //     fill(255, 0, 0);
+    //     //desenha o caminho melhor
+    //     caminho.map(way => {
+    //       let node = grid[way.x][way.y];
+    //       rect(node.x, node.y, node.t, node.t);
+    //       fill(0, 0, 255, 150);
+    //     });
+
 
     console.log("CHEGOU")
-    
+
     noLoop();
   }
 
   if (melhor.visitada) {
     melhor = ordenada[ordenada.indexOf(melhor) + 1]
   }
-
 
 
   grid[ladrao.pos.x - 1][ladrao.pos.y - 1].ladrao = false;
@@ -177,22 +170,18 @@ function Procurar(ladrao, pintura) {
 
 }
 
+function mouseClicked() {
 
+  if (cont < 2) {
+    var x = Math.floor(mouseX / size);
+    var y = Math.floor(mouseY / size);
 
+    personagens.push({
+      "x": x,
+      "y": y
+    });
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-function gerarPos(rows, cols) {
-  return createVector(
-    Math.round(random(rows)), Math.round(random(cols))
-  )
+  // prevent default
+  return false;
 }
